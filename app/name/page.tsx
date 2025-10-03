1459aec73f39428becd50729599ff5dd7805de1f
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-10-04 02:27:55 +0300

    feat: implement form handling with validation for first and last name fields

--- app/name/page.tsx
+++ app/name/page.tsx
@@ -1,22 +1,77 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { CustomFormInput } from '@/components/ui/CustomFormInput';
 import { paths } from '@/lib/paths';
+import { formSchema, FormSchema } from '@/lib/schema';
 import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 const NamePage = () => {
   const router = useRouter();
 
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+    },
+  });
+
+  const submitForm = form.handleSubmit((data) => {
+    console.log(data);
+
+    router.push(paths.email);
+  });
+
   return (
-    <div className='space-y-4 rounded-md bg-white p-6 shadow-lg'>
-      <CustomFormInput label='Fist Name' />
+    <div className='rounded-md bg-white p-6 shadow-lg'>
+      <Form {...form}>
+        <form onSubmit={submitForm} className='space-y-4'>
+          <FormField
+            control={form.control}
+            name='firstName'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
 
-      <CustomFormInput label='Last Name' />
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
 
-      <div className='text-end'>
-        <Button onClick={() => router.push(paths.email)}>Next</Button>
-      </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='lastName'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className='text-end'>
+            <Button type='submit'>Next</Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
