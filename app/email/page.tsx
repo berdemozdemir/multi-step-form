12ae14a2cf4610fd09fffc546c3e7f1be87064c6
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-10-04 02:35:13 +0300

    feat: implement email form with validation and navigation to password page

--- app/email/page.tsx
+++ app/email/page.tsx
@@ -1,19 +1,61 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { CustomFormInput } from '@/components/ui/CustomFormInput';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { formSchema, FormSchema } from '@/lib/schema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
 
 const EmailPage = () => {
   const router = useRouter();
 
-  return (
-    <div className='space-y-4 rounded-md bg-white p-4 shadow-lg'>
-      <CustomFormInput label='E-mail' />
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
 
-      <div className='text-end'>
-        <Button onClick={() => router.push('/password')}>Next</Button>
-      </div>
+  const submitForm = form.handleSubmit((data) => {
+    console.log(data);
+
+    router.push('/password');
+  });
+
+  return (
+    <div className='rounded-md bg-white p-4 shadow-lg'>
+      <Form {...form}>
+        <form className='space-y-4' onSubmit={submitForm}>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className='text-end'>
+            <Button type='submit'>Next</Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
