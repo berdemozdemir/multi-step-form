b3b8c1023c29fdea65c9ad71c1a78a8d18a259cc
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-10-05 03:38:54 +0300

    feat: update form schema to require fields and add password confirmation validation

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,11 +1,16 @@
 import z from 'zod';
 
-export const formSchema = z.object({
-  firstName: z.string().min(3).optional(),
-  lastName: z.string().min(3).optional(),
-  email: z.email().optional(),
-  password: z.string().optional(),
-  confirmPassword: z.string().optional(),
-});
+export const formSchema = z
+  .object({
+    firstName: z.string().min(3),
+    lastName: z.string().min(3),
+    email: z.email(),
+    password: z.string(),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  });
 
 export type FormSchema = z.infer<typeof formSchema>;
