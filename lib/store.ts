e3f0034b0f114842b2e982e7fd0b6bc025dbc178
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-10-05 03:58:33 +0300

    feat: refactor initial state setup and clearState action in form store for improved clarity

--- lib/store.ts
+++ lib/store.ts
@@ -6,18 +6,26 @@ type FormState = FormSchema;
 
 type FromAction = {
   setData: (data: Partial<FormSchema>) => void;
+
+  clearState: () => void;
+};
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
 };
 
 export const useFormStore = create(
   persist<FormState & FromAction>(
     (set) => ({
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      ...initialState,
 
       setData: (data) => set((state) => ({ ...state, ...data })),
+
+      clearState: () => set(() => ({ ...initialState })),
     }),
     { name: 'form-store', storage: createJSONStorage(() => localStorage) }
   )
