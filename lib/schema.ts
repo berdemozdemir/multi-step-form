ec521d9bee82f9422523a2c36c0a49a1780ab18e
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-10-05 03:40:17 +0300

    feat: enhance password validation with regex for stronger security

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,11 +1,19 @@
 import z from 'zod';
 
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+
 export const formSchema = z
   .object({
     firstName: z.string().min(3),
     lastName: z.string().min(3),
     email: z.email(),
-    password: z.string(),
+    password: z
+      .string()
+      .regex(
+        passwordRegex,
+        '        Password must include uppercase, lowercase, number, and special character'
+      ),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
